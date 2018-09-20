71c0a3f6ad66254de670a1f4a1b51e304636f197
mymindd <mypspgood@gmail.com>
2018-09-20 11:26:38 +0700

    use handler

--- src/WordCard.js
+++ src/WordCard.js
@@ -3,13 +3,12 @@ import './App.css';
 import CharacterCard from './CharacterCard';
 
 export default class WordCard extends Component{
+    activationHandler = c => {console.log(`${c} has been activated.`)}
+
+     //<CharacterCard value={c} key={i} activationHandler={this.activationHandler}/>
     render(){
         return(
-            <div>
-            {
-                Array.from(this.props.value).map((c, i)=> <CharacterCard value={c} key={i}/>)
-              }
-            </div>
+            <CharacterCard value={c} key={i} activationHandler={this.activationHandler}/>
         );
     }
 }
