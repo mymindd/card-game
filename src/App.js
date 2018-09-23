809a4864b118e437088c6382ed3dd6cc87524c0c
somboon changkhom <mypspgood@gmail.com>
2018-09-24 00:03:36 +0700

    change background when active

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <WordCard value="HELLO"/>
+        <WordCard value={"HELLO"}/>
          </div>
       );
   }
